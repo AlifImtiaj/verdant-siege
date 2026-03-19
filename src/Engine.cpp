669bb66b7ad6e60dc9c95c2bc0225385d27d2918
Alif Imtiaj <lolalif412@gmail.com>
2026-03-19 23:24:12 +0600

    Add imgui

--- src/Engine.cpp
+++ src/Engine.cpp
@@ -1,10 +1,11 @@
 #include "Engine.h"
 
+#include <imgui.h>
+#include <imgui-SFML.h>
+
 Engine::Engine(unsigned int width, unsigned int height, const std::string &title) :
 _window(sf::VideoMode({width, height}), title),
  _world(_inputHandler, _window) {
-
-
     _windowHeight = height;
     _windowWidth = width;
     bGameRunning = true;
@@ -15,7 +16,13 @@ _window(sf::VideoMode({width, height}), title),
 }
 
 void Engine::Run() {
+    if (!ImGui::SFML::Init(_window)) {
+        printf("ImGui Init failed\n");
+        bGameRunning = false;
+        return;
+    }
     Start();
+
 
     while(bGameRunning) {
         HandleInput();
@@ -31,15 +38,31 @@ void Engine::Start() {
 
 void Engine::HandleInput() {
     while (std::optional<sf::Event> event = _window.pollEvent()) {
+        ImGui::SFML::ProcessEvent(_window, *event);
         if (event->getIf<sf::Event::Closed>()) {
             bGameRunning = false;
         }
     }
 }
 
+// bool bFullScreen = false;
+// bool bWindowLastState = true;
+
 void Engine::Update() {
-    float dt = _gameClock.restart().asSeconds();
+    sf::Time deltaTime = _gameClock.restart();
+    float dt = deltaTime.asSeconds();
     _inputHandler.Update(_window);
+    ImGui::SFML::Update(_window, deltaTime);
+
+    // bWindowLastState = bFullScreen;
+    // ImGui::Begin("Window");
+    // ImGui::Checkbox("Fullscreen", &bFullScreen);
+    // ImGui::End();
+    // if (bWindowLastState != bFullScreen) {
+    //     bFullScreen ? _window.create(sf::VideoMode({_windowWidth, _windowHeight}),"Game Window", sf::State::Fullscreen) :
+    //     _window.create(sf::VideoMode({_windowWidth, _windowHeight}),"Game Window", sf::State::Windowed);
+    // }
+    
 
     _world.Update(dt);
 }
@@ -47,12 +70,15 @@ void Engine::Update() {
 void Engine::Render() {
     
     _window.clear();
-    // render everything here
+    
     _world.Render();
 
+    // imgui should always render at last
+    ImGui::SFML::Render(_window);
     _window.display();
 }
 
 Engine::~Engine() {
+    ImGui::SFML::Shutdown();
     _window.close();
 }
